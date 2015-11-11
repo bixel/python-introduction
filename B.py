60625877a23e26e66c2c97cbeb4f139ede717eda
Kevin Heinicke <kevin@kehei.de>
2015-11-11 15:07:13 +0100

    Use numpy for readin and add errorbars.

--- B.py
+++ B.py
@@ -3,17 +3,18 @@
 
 from collections import namedtuple
 import matplotlib.pyplot as plt
+import numpy as np
 
 BCand = namedtuple('BCand', ['m', 'merr', 'pt', 'p'])
 
-bs = []
-
-with open('B.txt') as f:
-    for line in f.readlines()[1:]:
-        bs.append(BCand(*[float(v) for v in line.strip().split(',')]))
+bs = [BCand(*b) for b in np.genfromtxt('B.txt', skip_header=1, delimiter=',')]
 
 masses = [b.m for b in bs]
 
-plt.hist(masses, 60, histtype='stepfilled')
+ns, bins, _ = plt.hist(masses, 60, histtype='stepfilled', facecolor='r',
+                       edgecolor='none')
+centers = bins[:-1] + (bins[1:] - bins[:-1]) / 2
+merr = np.sqrt(ns)
+plt.errorbar(centers, ns, yerr=merr, fmt='b+')
 plt.xlabel(r'$m_B / \mathrm{GeV}$')
 plt.savefig('mass.pdf')
